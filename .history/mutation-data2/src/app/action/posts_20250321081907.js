a6a013b7476f8499a2a7920716d52ff2d88685c2
Your Name <1821503@iub.edu.bd>
2025-03-21 08:31:35 +0600

    Initial commit or update

---
+++ .history/mutation-data2/src/app/action/posts_20250321081907.js
@@ -0,0 +1,50 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+import { storePost, updatePostLikeStatus } from "../lib/posts";
+import { revalidatePath } from "next/cache";
+import { uploadImage } from "../lib/cloudinary";
+
+export async function createPost(prevState,formData) {
+       
+        const title = formData.get("title");
+        const image = formData.get("image");
+        const content = formData.get("content");
+
+        let errors=[];
+        if(!title || title.trim().length ===0){
+            errors.push("title is required.");
+
+        }
+        if(!content || content.trim().length===0){
+            errors.push("content is required.");
+        }
+        if(!image || image.size===0){
+            errors.push("image is required.");
+        }
+        if(errors.length>0){
+            return {errors};
+
+        }
+        let imageUrl;
+        try{
+            imageUrl = await uploadImage(image);
+        } catch(error){
+            throw new Error('Image upload failed.post was not created. please try again later.')
+        }
+        
+        await storePost({
+            imageUrl: '',
+            title,
+            content,
+            userId: 1,
+         });
+         redirect('/feed');
+      }
+
+      export async function togglePostLikeStatus(postId){
+       await updatePostLikeStatus(postId,2);
+       revalidatePath('/','layout');
+
+      }
---
+++ .history/mutation-data2/src/app/feed/error_20250321081438.js
@@ -0,0 +1,9 @@
+
+export default function FeedError(){
+    return(
+        <>
+        <h1>An error occured!</h1>
+        <p>Unfortunately, something went wrong.</p>
+        </>
+    )
+}
